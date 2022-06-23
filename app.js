4972b300b3a0a14ff6d1fff84d56d235da09223c
Giovanni Clayton <giovanni.gcda@gmail.com>
2022-06-23 11:41:57 -0300

    feat: criando models user

--- app.js
+++ app.js
@@ -6,10 +6,30 @@ const jwt = require("jsonwebtoken");
 
 const app = express();
 
+//config json response
+app.use(express.json())
+
+//models
+const User = require("./models/User")
+
+//rota inicial
 app.get("/", (req, res) => {
   res.status(200).json({ mensagem: "Bem vindo a nossa API!" });
 });
 
+//rota de registro de usuario
+app.post("/auth/register", async (req, res) => {
+
+    const{name,email,password,confirmPassword} = req.body
+
+    //validações
+    if(!name) {
+        return res.status(422).json({mensagem: "O nome é obrigatório!"})
+    }
+
+})
+
+//credenciais de acesso ao banco de dados
 const dbUser = process.env.DB_USER 
 const dbPassword = process.env.DB_PASS
 
